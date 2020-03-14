e094c386cb9c997ee76accbc9bd817fec8d83d11
titarakshay <titarakshay@gmail.com>
2020-03-14 14:46:36 +0530

    music-app

---
+++ main.js
@@ -0,0 +1,18 @@
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const colors=[
+      '#60d394',
+      '#d36060',
+      '#c060d3',
+      '#d3d160',
+      '#606bd3',
+      '#60c2d3'
+    ]
+  
+    pads.forEach((pad, index) => {
+      pad.addEventListener("click", function() {
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        document.body.style.backgroundColor=colors[index]
+      });
+    });
